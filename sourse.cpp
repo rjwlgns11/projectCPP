c64cc5e8ef346b7fe28dc8245564723f852ae6a9
rjwlgns11 <163695232+rjwlgns11@users.noreply.github.com>
2024-03-26 04:26:52 +0900

    Initial commit

---
+++ sourse.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+void binary(int num);
+void CollatzConjecture(int num);
+
+
+int main()
+{
+	int a;
+	cout << "2진법으로 변환할 정수를 입력하세요" << endl;
+	cin >> a;
+	binary(a);
+	cout << endl;
+
+	cout << "콜라츠 추측을 확인할 정수를 입력하세요" << endl;
+	cin >> a;
+	CollatzConjecture(a);
+
+
+	return 0;
+}
+
+void binary(int num)
+{
+	bool check = num == 0 || num == 1;
+
+	if (check)
+	{
+		cout << num;
+	}
+	else
+	{
+	    binary(num / 2);
+		cout << num%2;
+	}
+
+}
+
+void CollatzConjecture(int num)
+{
+
+	if (num % 2 == 0)//짝수인경우
+	{
+		cout << num << endl;
+		CollatzConjecture(num / 2);
+
+		
+	}
+	else if (num == 1)//반복하다 1이 남은 경우
+	{
+		cout << num << endl;
+		
+	}
+	else//홀수인경우
+	{
+		cout << num << endl;
+		CollatzConjecture(num * 3 + 1);
+	}
+}
